148c6d00cfe64fe5d71b8787e0c48c5dd364bbfe
vojta-horanek <vojtechhoranek@gmail.com>
2016-10-23 19:43:56 +0200

    Launcher shortcuts on Android 7.1 Updated slovak translation

--- app/src/main/java/cf/vojtechh/apkmirror/MainActivity.java
+++ app/src/main/java/cf/vojtechh/apkmirror/MainActivity.java
@@ -15,6 +15,7 @@ import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
